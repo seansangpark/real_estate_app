3402800c081db0d54e4fc65f37a13ac846b46f7b
Sean Park <seansangpark@gmail.com>
2023-09-13 17:46:44 -0400

    new component Property rendering

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+import Property from '../components/Property';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 const Banner = ({
