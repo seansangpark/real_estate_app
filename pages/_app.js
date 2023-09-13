5787a5950d67cda8c0d6be4ddd3e54d349f1429a
Sean Park <seansangpark@gmail.com>
2023-09-13 18:05:41 -0400

    layout components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgresss from 'nprogress';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
