a3d961802ac5bbe1d78a5fa11b75f47087e8e4ac
Sean Park <seansangpark@gmail.com>
2023-09-13 17:55:04 -0400

    rendering price

--- components/Property.jsx
+++ components/Property.jsx
@@ -6,6 +6,8 @@ import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
 
+import DefaultImage from '../assets/images/house.jpg';
+
 const Property = ({
   property: {
     coverPhoto,
@@ -24,7 +26,46 @@ const Property = ({
     href={`/property/${externalId}`}
     passHref
   >
-    {title}
+    <Flex
+      flexWrap='wrap'
+      w='420px'
+      p='5'
+      paddingTop='0'
+      justifyContent='flex-start'
+      cursor='pointer'
+    >
+      <Box>
+        <Image
+          src={coverPhoto ? coverPhoto.url : DefaultImage}
+          width={400}
+          height={260}
+          alt='house'
+        />
+      </Box>
+      <Box w='full'>
+        <Flex
+          paddingTop='2'
+          alignItems='center'
+          justifyContent='space-between'
+        >
+          <Flex alignItems='center'>
+            <Box
+              paddingRight='3'
+              color='green.400'
+            >
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text
+              fontWeight='bold'
+              fontSize='lg'
+            >
+              AED {price}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
+    </Flex>
   </Link>
 );
 
