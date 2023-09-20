81da57c8863f0ca1c06fa4fc7ce4800f66a25969
Sean Park <seansangpark@gmail.com>
2023-09-20 13:54:41 -0400

    image slider bug killed

--- components/ImageScrollBar.jsx
+++ components/ImageScrollBar.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import Image from 'next/image';
 import { Box, Icon, Flex } from '@chakra-ui/react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 
 const LeftArrow = () => {
