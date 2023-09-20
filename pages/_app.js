514c7be519bbee9df9c891540dd82feae4811e69
Sean Park <seansangpark@gmail.com>
2023-09-20 13:59:15 -0400

    completed

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,27 @@ import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
+  NProgresss.configure({ showSpinner: false });
+
+  Router.events.on('routeChangeStart', () => {
+    NProgresss.start();
+  });
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgresss.done();
+  });
+
   return (
     <>
-      <Head></Head>
+      <Head>
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
+          integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=='
+          crossOrigin='anonymous'
+          referrerPolicy='no-referrer'
+        />
+      </Head>
       <ChakraProvider>
         <Layout>
           <Component {...pageProps} />
